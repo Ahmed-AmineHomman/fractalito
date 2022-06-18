324a7f18f46e57cd407fecc5b680a86f694c02cd
ahmed <107698647+Ahmed-AmineHomman@users.noreply.github.com>
2022-06-18 20:04:35 +0200

    adding argument parser

--- run.py
+++ run.py
@@ -1,29 +1,101 @@
 import matplotlib.pyplot as plt
 
+import argparse
+import sys
+
 from fractalito import compute_image
 from fractalito.operators import Mandelbrot as Core
 
 
+def get_parameters(parameters):
+    parser = argparse.ArgumentParser(description="fractalito's argument parser")
+    parser.add_argument(
+        "--xmin",
+        nargs=1,
+        type=float,
+        default=-2.0,
+        help="the minimal considered value in the x-axis when computing the fractal"
+    )
+    parser.add_argument(
+        "--xmax",
+        nargs=1,
+        type=float,
+        default=2.0,
+        help="the maximal considered value in the x-axis when computing the fractal"
+    )
+    parser.add_argument(
+        "--ymin",
+        nargs=1,
+        type=float,
+        default=-2.0,
+        help="the minimal considered value in the y-axis when computing the fractal"
+    )
+    parser.add_argument(
+        "--ymax",
+        nargs=1,
+        type=float,
+        default=2.0,
+        help="the maximal considered value in the y-axis when computing the fractal"
+    )
+    parser.add_argument(
+        "--xres",
+        nargs=1,
+        type=int,
+        default=100,
+        help="x-resolution (in pixels) of the fractal image"
+    )
+    parser.add_argument(
+        "--yres",
+        nargs=1,
+        type=int,
+        default=100,
+        help="y-resolution (in pixels) of the fractal image"
+    )
+    parser.add_argument(
+        "--parameter",
+        nargs=1,
+        type=str,
+        default=1j,
+        help="parameter value of the fractal's associated equation"
+    )
+    parser.add_argument(
+        "--boundary",
+        nargs=1,
+        type=float,
+        default=2.0,
+        help="divergence threshold, i.e. the value corresponding to the maximal absolute value a sequence can have"
+             "before being considered as divergent."
+    )
+    return parser.parse_args(parameters)
+
+
 if __name__ == "__main__":
-    ensemble = 'mandelbrot'
-    limits = {"x": (-2.5, 2.5), "y": (-2.0, 2.0)}
-    resolution = {"x": 100, "y": 100}
-    parameter = 1j
-    boundary = 2
-    operator = Core()
+    # parse parameters
+    parameters = get_parameters(parameters=sys.argv[1:])
+    print("PARAMETERS:")
+    for key, value in vars(parameters).items():
+        print(f"  - {key:12s}: {value}")
 
     print("computing fractal.")
-    heatmap = compute_image(operator=operator,
-                            limits=limits,
-                            parameter=parameter,
-                            resolution=resolution,
+    heatmap = compute_image(operator=Core(),
+                            limits={"x": (parameters.xmin, parameters.xmax),
+                                    "y": (parameters.ymin, parameters.ymax)},
+                            parameter=complex(parameters.parameter),
+                            resolution={"x": parameters.xres, "y": parameters.yres},
                             max_iterations=100,
-                            boundary=boundary)
+                            boundary=parameters.boundary)
 
     print("drawing image.")
     fig = plt.figure()
     ax = fig.add_subplot()
-    ax.imshow(heatmap, vmin=0., vmax=1.0, cmap='gray', interpolation=None, extent=(*limits["x"], *limits["y"]))
+    ax.imshow(
+        heatmap,
+        vmin=0.,
+        vmax=1.0,
+        cmap='gray',
+        interpolation=None,
+        extent=(parameters.xmin, parameters.xmax, parameters.ymin, parameters.ymax)
+    )
     ax.set_xticks([])
     ax.set_yticks([])
     plt.show()
